338784e6bca4e4e7df961350b9ba42e269df12f7
codeandsmile1 <codeandsmile@gmail.com>
2020-11-15 18:44:13 +0200

    added basic map

--- js/script.js
+++ js/script.js
@@ -3,7 +3,17 @@ const address = document.getElementById("address");
 const email = document.getElementById("email");
 const phone = document.getElementById("phone");
 const website = document.getElementById("website");
+const mapId = document.getElementById("map");
 
+let map;
+
+function initMap() {
+    map = new google.maps.Map(mapId, {
+      center: { lat: -34.397, lng: 150.644 },
+      zoom: 8,
+    });
+    console.log(map)
+  }
 
 console.log("my App")
 
