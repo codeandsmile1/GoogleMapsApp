c0cd22068cd5f5f73665987c02f22bef49dea988
codeandsmile1 <codeandsmile@gmail.com>
2020-11-15 18:10:38 +0200

    basic structure of the form

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
+const name = document.getElementById("name");
+const address = document.getElementById("address");
+const email = document.getElementById("email");
+const phone = document.getElementById("phone");
+const website = document.getElementById("website");
+
+
 console.log("my App")
+
